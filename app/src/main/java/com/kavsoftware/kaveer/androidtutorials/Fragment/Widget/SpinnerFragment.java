93652273cc4132b5d51600f0aa7c178cab8a1dc3
kaveer <kaveer.rajcoomar@gmail.com>
2017-11-22 14:42:47 +0400

    add all widgets

---
+++ app/src/main/java/com/kavsoftware/kaveer/androidtutorials/Fragment/Widget/SpinnerFragment.java
@@ -0,0 +1,19 @@
+package com.kavsoftware.kaveer.androidtutorials.Fragment.Widget;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kavsoftware.kaveer.androidtutorials.R;
+
+public class SpinnerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_spinner, container, false);
+    }
+}
